d08536dec6cba96529a6eb084d78ff967b8d577b
kadrick <kbk2581553@gmail.com>
2021-09-25 15:04:17 +0900

    update lv1.py

--- lv1.py
+++ lv1.py
@@ -1,3 +1,9 @@
+'''''''''''''''''''''''''''''
+> Filename: lv1.py
+> Author: Kadrick, BoGwon Kang
+> Created at: -
+> Description: String
+'''''''''''''''''''''''''''''
 def findKey(input):
     for i in range(0, len(input)):
         if input[i] == " ":
@@ -11,7 +17,24 @@ def findKey(input):
         else:
             output = input[i]
         print(output, end="")
-        
-findKey("g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj.")
+
+def adjustMethod(input, srcTable, destTable):
+    transTable = str.maketrans(srcTable, destTable)
+    return input.translate(transTable)
+       
+statement = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
+
+print("by loop")
+findKey(statement)
 print("")
 findKey("map")
+
+src = dest = ""
+for i in range(0, 26):
+    src += chr(i + ord("a"))
+    dest += chr((i + 2)  % 26 + ord("a"))
+
+print("")
+print("by string.maketrans()")
+print(adjustMethod(statement, src, dest))
+print(adjustMethod("map", src, dest))
